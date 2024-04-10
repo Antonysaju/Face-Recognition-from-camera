bfa22543573cad0edf5caec6fe734b4083fa480e
Antony Saju David <74053283+Antonysaju@users.noreply.github.com>
2024-04-10 23:49:15 +0530

    Add files via upload

--- Face_Recognition_from_cam.py
+++ Face_Recognition_from_cam.py
@@ -4,25 +4,20 @@ import numpy as np
 
 video_capture= cv2.VideoCapture(0)
 
-face1= face_recognition.load_image_file("untitled.png")
+face1= face_recognition.load_image_file("antony.jpg")
 face1_encoding= face_recognition.face_encodings(face1)[0]
 
-face2= face_recognition.load_image_file("untitled2.jpg")
+face2= face_recognition.load_image_file("lakshman.jpeg")
 face2_encoding= face_recognition.face_encodings(face2)[0]
-
-face3= face_recognition.load_image_file("untitled3.jpg")
-face3_encoding= face_recognition.face_encodings(face3)[0]
 
 known_face_encodings= [
     face1_encoding,
-    face2_encoding,
-    face3_encoding
+    face2_encoding
 ]
 
 known_face_names=[
-    "Antony Saju David",
-    "Saju David",
-    "Simy Saju"
+    "Antony",
+    "Lakshman"
 ]
 
 face_locations= []
@@ -31,39 +26,44 @@ face_names= []
 process_this_frame= True
 
 while True:
-    ret,frame= video_capture.read()
-    small_frame= cv2.resize(frame,(0,0), fx= 0.25, fy= 0.25)
-    rgb_small_frame = cv2.cvtColor(small_frame,cv2.COLOR_BGR2RGB)
+    ret, frame = video_capture.read()
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
     if process_this_frame:
-        face_locations= face_recognition.face_locations(rgb_small_frame)
-        face_encodings= face_recognition.face_encodings(rgb_small_frame, face_locations)
-        face_names= []
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+        face_names = []
         for face_encoding in face_encodings:
-            matches= face_recognition.compare_faces(known_face_encodings, face_encoding)
-            name= "Unknown"
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = "Unknown"
 
-            face_distances= face_recognition.face_distance(known_face_encodings, face_encoding)
-            best_match_index= np.argmin(face_distances)
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
             if matches[best_match_index]:
-                name= known_face_names[best_match_index]
+                name = known_face_names[best_match_index]
             face_names.append(name)
-    process_this_frame= not process_this_frame
-
+    process_this_frame = not process_this_frame
 
     for (top, right, bottom, left), name in zip(face_locations, face_names):
         top *= 4
         right *= 4
         bottom *= 4
-        left*= 4
+        left *= 4
 
-        cv2.rectangle(frame,(left,top),(right,bottom),(0,0,255),2)
-        cv2.rectangle(frame,(left,bottom-35),(right,bottom),(0,0,255),cv2.FILLED)
-        font=cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame,name,(left+6,bottom-6),font,1.0,(255,255,255),1) 
+        if name != "Unknown":
+            color = (0, 255, 0)
+        else:
+            color = (0, 0, 255)
+
+        cv2.rectangle(frame, (left, top), (right, bottom+20), color, 2)
+        cv2.rectangle(frame, (left, bottom - 15), (right, bottom+20), color, cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom + 12), font, 1.0, (255, 255, 255), 1)
 
     cv2.imshow("Video", frame)
 
-    if cv2.waitKey(1) & 0xFF==ord("q"):
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
+
 video_capture.release()
 cv2.destroyAllWindows()
